ef14b7ae8acaeb9ef87ee5e8ba1cd98e41badfbd
fysal <fysal005@gmail.com>
2022-05-04 12:06:04 +0300

    account

--- src/components/Nav/MainNav.jsx
+++ src/components/Nav/MainNav.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Input from '../form/widgets/Input';
-import styles from './styles/styles.module.css';
-import clsx from 'clsx';
-import logo from '../../assets/logo/teddy-logo.png';
-import shopping_cart from '../../assets/icons/shopping-cart.png';
+import React from "react";
+import Input from "../form/widgets/Input";
+import styles from "./styles/styles.module.css";
+import clsx from "clsx";
+import logo from "../../assets/logo/teddy-logo.png";
+import shopping_cart from "../../assets/icons/shopping-cart.png";
 
 const MainNav = () => {
   return (
@@ -14,14 +14,17 @@ const MainNav = () => {
             <img src={logo} width="70" />
           </div>
         </div>
-        <div className="col-sm-12 col-md-6 ">
+        <div className="col-sm-12 col-md-5 ">
           <div
             className={clsx(
               styles.nav_search,
               "d-flex align-center justify-content-start flex-1"
             )}
           >
-            <input placeholder="Search..." className={clsx(styles.input,"ms-2")} />
+            <input
+              placeholder="Search..."
+              className={clsx(styles.input, "ms-2")}
+            />
             <span
               className={clsx(styles.btnSearch, "material-icons-outlined ms-2")}
             >
@@ -42,25 +45,43 @@ const MainNav = () => {
             </div>
           </div>
         </div>
-        <div className="col-sm-12 col-md-2">
+        <div className="col-sm-12 col-md-3 ">
           <div className="d-flex align-items-center">
-            <span className={clsx(styles.cart_, "position-relative")}>
-              <img src={shopping_cart} width="20" />
-              <span
-                className={clsx(
-                  styles.count,
-                  "d-flex align-items-center justify-content-center text-white rounded-circle"
-                )}
-              >
-                0
+            <div className="float-start d-flex align-items-center flex-2">
+              <span className={clsx(styles.cart_, "position-relative")}>
+                <img src={shopping_cart} width="20" />
+                <span
+                  className={clsx(
+                    styles.count,
+                    "d-flex align-items-center justify-content-center text-white rounded-circle"
+                  )}
+                >
+                  0
+                </span>
               </span>
-            </span>
-            <span className={clsx(styles.catx, "ms-3 ddfdf")}>My cart</span>
+              <span className={clsx(styles.catx, "ms-3")}>My Cart</span>
+            </div>
+            <div className="d-flex align-items-center ms-3 pointer">
+              <span
+                className="material-icons-outlined "
+                style={{ fontSize: "30px", opacity: ".6" }}
+              >
+                <span class="material-symbols-outlined">
+                  account_circle
+                </span>
+              </span>
+              <span
+                className="ms-2 small"
+                style={{ fontWeight: "500", opacity: ".7" }}
+              >
+                Account
+              </span>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default MainNav
+export default MainNav;
