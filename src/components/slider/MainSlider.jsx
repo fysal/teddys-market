1df14387dab66e02e364dc1240eff371508ce503
fysal <fysal005@gmail.com>
2022-05-22 11:49:30 +0300

    Implemented slicer

--- src/components/slider/MainSlider.jsx
+++ src/components/slider/MainSlider.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import slider from '../../assets/slider/teddy-slider1.png';
+import React, { useEffect } from "react";
+import slide1 from "../../assets/slider/teddy-slider1.png";
+import slide2 from "../../assets/slider/teddy-slider2.jpg";
+import Carousel from "nuka-carousel";
 
 const MainSlider = () => {
-  return (
-    <div className="slider">
-      <img src={slider} width="100%" />
-    </div>
-  );
-}
+  const slides = [slide1, slide2];
 
-export default MainSlider
+  useEffect(() => {
+    let nextBtn = document.querySelector('[aria-label="next"]');
+    let prevBtn = document.querySelector('[aria-label="previous"]');
+    nextBtn.innerHTML = '<span class="material-icons-outlined">east</span>';
+    prevBtn.innerHTML = '<span class="material-icons-outlined">west</span>';
+  }, []);
+
+  return (
+    <Carousel wrapAround={true} autoplay withoutControls={false}>
+      {slides.map((slide, index) => (
+        <div className="slider" style={{ maxHeight: "584px" }} key={index}>
+          <img src={slide} width="100%" />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export default MainSlider;
