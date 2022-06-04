15af81513a9c7a8fba75033befd439c86444a68f
fysal <fysal005@gmail.com>
2022-06-05 00:37:23 +0300

    MOdified searchbar

--- src/components/Nav/widgets/SearchBar.jsx
+++ src/components/Nav/widgets/SearchBar.jsx
@@ -4,11 +4,13 @@ import React from "react";
 import styles from "../styles/styles.module.css";
 import { useHistory } from "react-router-dom";
 
+
 const SearchBar = () => {
   const [searchText, setSearchText] = useState("");
   const history = useHistory();
 
-  const onSearch = () => {
+  const onSearch = (e) => {
+    e.preventDefault();
     if (searchText !== "") {
       history.push({
         pathname: "/products",
@@ -20,7 +22,8 @@ const SearchBar = () => {
   };
   const onChange = (e) => setSearchText(e.target.value);
   return (
-    <div
+    <form
+      onSubmit={(e) => onSearch(e)}
       className={clsx(
         styles.nav_search,
         "d-flex align-center justify-content-start flex-1"
@@ -32,16 +35,17 @@ const SearchBar = () => {
         onChange={onChange}
         value={searchText}
       />
-      <span
-        onClick={() => onSearch()}
+      <button
+        type="submit"
         className={clsx(
           styles.btnSearch,
           "material-icons-outlined ms-2 pointer"
         )}
+        style={{border:"medium none", color:"#fff"}}
       >
         search
-      </span>
-    </div>
+      </button>
+    </form>
   );
 };
 
